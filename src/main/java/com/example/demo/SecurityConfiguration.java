4e2c05acf1c70ab192104098c61f1762586af66e
nmerris <nate.merris@gmail.com>
2017-09-05 14:11:01 -0400

    modify security config class

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,47 +1,80 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().loginPage("/login").permitAll()
-////                .antMatchers("/").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-////                .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
-//                .and().httpBasic();
-//    }
 
+    @Autowired
+    private SSUserDetailsService userDetailsService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return new SSUserDetailsService(userRepository);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-                .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
-                .anyRequest().authenticated()
-                .and().formLogin().loginPage("/login").permitAll()
-                .and().httpBasic();
+        http
+            .authorizeRequests()
+            .antMatchers("/", "/h2-console/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().loginPage("/login").permitAll()
+            .and()
+            .logout()
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            .logoutSuccessUrl("/login").permitAll().permitAll()
+            .and()
+            .httpBasic();
+
+        http
+            .csrf().disable();
+
+        http
+            .headers().frameOptions().disable();
+
+
+//                .access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+//            .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
+//            .anyRequest().authenticated()
+//            .and().httpBasic();
+//
+//
+
+
+        //        http.authorizeRequests()
+//                .antMatchers("/").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+//                .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
+//                .anyRequest().authenticated()
+//                .and().formLogin().loginPage("/login").permitAll()
+//                .and().httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().
-                withUser("user").password("pass").roles("USER")
+        auth
+            .userDetailsService(userDetailsServiceBean());
 
-                // to add more accounts, chain more
-                .and().withUser("userTwo").password("pass").roles("USER")
-
-                // add an admin user
-                .and().withUser("admin").password("pass").roles("ADMIN");
+//            .inMemoryAuthentication().
+//            withUser("user").password("pass").roles("USER")
+//
+//            // to add more accounts, chain more
+//            .and().withUser("userTwo").password("pass").roles("USER")
+//
+//            // add an admin user
+//            .and().withUser("admin").password("pass").roles("ADMIN");
 
 
     }
