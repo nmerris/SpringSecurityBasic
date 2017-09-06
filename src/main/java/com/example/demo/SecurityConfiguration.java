6a871f5596b3311df17d2c8d9f62d535bc369fd3
nmerris <nate.merris@gmail.com>
2017-09-06 12:59:15 -0400

    add registration.html

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -28,11 +30,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                // ROLE_ADMIN is NOT a special thing, it's just convention, the String here must exactly match
-                // whatever string you use when you create Roles and save them to role repo
+            // ROLE_ADMIN is NOT a special thing, it's just convention, the String here must exactly match
+            // whatever string you use when you create Roles and save them to role repo
             .antMatchers("/secure").access("hasRole('ROLE_ADMIN')")
             .antMatchers("/").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-            .antMatchers("/", "/setuprolesandusers").permitAll() // so anyone can get to default route and my manual testing route
+            .antMatchers("/", "/setuprolesandusers", "/register").permitAll() // so anyone can get to default route and my manual testing route
             .anyRequest().authenticated()
             .and()
             .formLogin().loginPage("/login").permitAll()
@@ -49,21 +51,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
             .headers().frameOptions().disable();
 
-
-//                .access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-//            .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
-//            .anyRequest().authenticated()
-//            .and().httpBasic();
-//
-//
-
-
-        //        http.authorizeRequests()
-//                .antMatchers("/").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
-//                .antMatchers("/admin").access("hasRole('ROLE_ADMIN')")
-//                .anyRequest().authenticated()
-//                .and().formLogin().loginPage("/login").permitAll()
-//                .and().httpBasic();
     }
 
     @Override
